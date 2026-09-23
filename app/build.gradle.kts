698c3a0062c8f0f22071ebc90dc8a32ad6c77f75
ohlmannfrieling-beep <ohlmannfrieling@gmail.com>
2026-09-23 23:54:10 +0200

    Bump Foto Finals to 0.6.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "de.photofinals.app"
         minSdk = 30
         targetSdk = 35
-        versionCode = 5
-        versionName = "0.5.0"
+        versionCode = 6
+        versionName = "0.6.0"
     }
 
     compileOptions {
